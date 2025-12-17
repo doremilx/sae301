4371e0f6992b10eca9abe9a07f74133b22912d62
aurelie-s06 <aurelie.sirot@gmail.com>
2025-12-17 11:14:08 +0100

    login

--- profile.php
+++ profile.php
@@ -1,43 +1,21 @@
+<?php 
+
+session_start();
+if (!isset($_SESSION['login'])) {
+    header('Location: connexion.php');
+    exit();
+}
+?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Profil</title>
 </head>
 <body>
-    <h1>Connexion</h1>
-    <form action="traite_login.php" method="post">
-        <input type="text" name="login" placeholder="Login">
-        <input type="password" name="password" placeholder="Password">
-        <input type="submit" value="Se connecter">
-    </form>
-
-    <h1>Créer un compte</h1>
-    <form action="creer_compte.php" method="POST">
-        <input type="text" name="nom" id="nom" placeholder="Nom">
-        <input type="text" name="prenom" id="prenom" placeholder="Prénom">
-        <input type="text" name="login" id="login" placeholder="Login">
-        <div style="display:flex; align-items:center; gap:5px;">
-    <input type="password" name="motdepasse" id="motdepasse" placeholder="Mot de passe">
-    <button type="button" id="toggleBtn" onclick="togglePassword()">👁️</button>
-<button type="submit">Créer un compte</button>
-
-</form>
-    <script>
-    function togglePassword() {
-    let input = document.getElementById("motdepasse");
-    let btn = document.getElementById("toggleBtn");
-
-    if (input.type === "password") {
-        input.type = "text";
-        btn.textContent = "🙈";
-    } else {
-        input.type = "password";
-        btn.textContent = "👁️";
-    }
-}
-
-</script>
+    <h1>Bienvenue sur votre profil, <?php echo htmlspecialchars($_SESSION['login']); ?> !</h1>
+    <p>Voici les informations de votre profil.</p>
+    <a href="logout.php">Se déconnecter</a>
 </body>
 </html>
