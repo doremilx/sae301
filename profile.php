72b2175789b4a5dda00812cfc3e9ffad1bbb2235
aurelie-s06 <aurelie.sirot@gmail.com>
2025-12-18 09:17:43 +0100

    profil

--- profile.php
+++ profile.php
@@ -31,8 +31,7 @@ $user = $stmt->fetch();
     <h2>Mon Tableau de bord CROUS</h2>
 
     <h3>Informations</h3>
-    <hr>
-    
+    <hr>  
     <form action="">
         <p>Modifier mes informations</p>
         <label for="nom">Nom</label>
@@ -43,10 +42,26 @@ $user = $stmt->fetch();
         <label for="ancienmdp">Mot de passe actuel</label>
         <input type="text" id="ancienmdp">
         <label for="nouveaumdp">Nouveau mot de passe</label>
-        <input type="text" id="nouveaumdp">
+        <input type="text" id="nouveaumdp"> 
         <br>
         <button>Sauvegarder les modifications</button>
     </form>
+
+    <h3>Mon Crous</h3>
+    <hr>  
+    <form action="">
+        <p>Modifier mes informations Izly</p>
+        <label for="email">Email</label>
+        <input type="text" id="email">
+        <label for="ncrous">Numéro carte Crous</label>
+        <input type="text" id="ncrous">
+        <br>
+        <button>Sauvegarder les modifications</button>
+    </form>
+
+    <h3>Historique</h3>
+    <hr> 
+
 
 </body>
 </html>
